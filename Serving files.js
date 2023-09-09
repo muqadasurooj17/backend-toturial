32f339d086da73db9dba2b88ffb0be771a4ba331
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-09 13:46:57 +0500

    added demo.html

--- Serving files.js
+++ Serving files.js
@@ -1,13 +1,16 @@
 const http=require('http')
 const fs=require('fs')
-const fileContent=fs.readFileSync('');
+
+const fileContent=fs.readFileSync('demo.html');
 
 const server= http.createServer((req,res)=>{
-    res.writeHead(200,{
-        'content-type':'text/html'
-    });
-    res.readableEnded(fileContent)
+    res.writeHead(200,
+    {'content-type':'text/html'}
+        );
+    res.end(fileContent)
 })
 server.listen(80,'127.0.0.1',()=>{
-    console.log("listening on port 80")
+    console.log("listening on port 80", )
 })
+//80 port means automaticaly srver will recognize the 
+//path
