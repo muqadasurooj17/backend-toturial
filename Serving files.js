64cdc58b0d9cbd081b945752661a9d497e8d6357
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-09 13:38:03 +0500

    serving files in nodejs

---
+++ Serving files.js
@@ -0,0 +1,13 @@
+const http=require('http')
+const fs=require('fs')
+const fileContent=fs.readFileSync('');
+
+const server= http.createServer((req,res)=>{
+    res.writeHead(200,{
+        'content-type':'text/html'
+    });
+    res.readableEnded(fileContent)
+})
+server.listen(80,'127.0.0.1',()=>{
+    console.log("listening on port 80")
+})
--- modules in js.js
+++ modules in js.js
@@ -2,3 +2,5 @@ const fs=require("fs");
 const text=fs.readFileSync
 ("List of global variables.txt","utf-8");
 console.log(text);
+//syncrhous and asynchrous 
+//line by line and block by block
